44b7a5f6c93d0d906fc3a94a5d8f0fce385d0586
이진실 <ljsil1204@naver.com>
2022-02-21 10:15:39 +0900

    페이지별 제목을, ViewPageAdapter에서 추가 오버라이딩으로 설정

--- app/src/main/java/com/neppplus/viewpager_fragment_20220218/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/neppplus/viewpager_fragment_20220218/adapters/MainViewPagerAdapter.kt
@@ -6,6 +6,15 @@ import androidx.fragment.app.FragmentPagerAdapter
 import com.neppplus.viewpager_fragment_20220218.fragments.*
 
 class MainViewPagerAdapter (fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when (position) {
+            0 -> "이름"
+            1 -> "출생년도"
+            else -> "인사말"
+        }
+    }
+
     override fun getCount() = 3
 
     override fun getItem(position: Int): Fragment {
@@ -15,7 +24,6 @@ class MainViewPagerAdapter (fm: FragmentManager) : FragmentPagerAdapter(fm) {
              1 -> BirthYearFragment()
              else -> HelloFragment()
          }
-
 
     }
 }
